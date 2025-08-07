10a9e27bd3e0bff1f79608d1a8eb40b8bd30f9cc
Viva-avv <vivachannn@gmail.com>
2025-08-07 22:23:08 +0800

    membuat website dessert bar kawaii tropi

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// toggel class active
+
+const navbarNav = document.querySelector ('.navbar-nav');
+//ketika menu di klik
+document.querySelector('#hamburger-menu').
+onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik diluar sidebar untuk menghilangkan nav
+
+const menu = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function(e) {
+    if(!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
+
+
